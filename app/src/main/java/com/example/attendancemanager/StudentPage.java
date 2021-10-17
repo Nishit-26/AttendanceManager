ace3ff2cf2e5a681b10164700fbf9cb43a1bd78c
Nishit <nishitpatel2681@gmail.com>
2021-10-17 14:32:54 +0530

    work in progress ...

--- app/src/main/java/com/example/attendancemanager/StudentPage.java
+++ app/src/main/java/com/example/attendancemanager/StudentPage.java
@@ -2,7 +2,9 @@ package com.example.attendancemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class StudentPage extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class StudentPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.student_page);
+
+        ImageView back;
+
+        back = findViewById(R.id.ivBack);
+        back.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), Home.class));
+            finish();
+        });
     }
 }
