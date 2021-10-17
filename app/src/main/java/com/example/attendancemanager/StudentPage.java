f6c735b46e758b1453511e03e41ab8e8da551335
Nishit <nishitpatel2681@gmail.com>
2021-10-17 13:50:58 +0530

    work in progress ...

---
+++ app/src/main/java/com/example/attendancemanager/StudentPage.java
@@ -0,0 +1,14 @@
+package com.example.attendancemanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StudentPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.student_page);
+    }
+}
--- app/src/main/java/com/example/attendancemanager/UserProfile.java
+++ app/src/main/java/com/example/attendancemanager/UserProfile.java
@@ -2,24 +2,37 @@ package com.example.attendancemanager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class UserProfile extends AppCompatActivity {
 
     ImageView back;
+    Button btnLogout;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
 
-        //Back Button part
+        //Hooks
         back = findViewById(R.id.ivBack);
+        btnLogout = findViewById(R.id.Logout);
+
+        //Back Button part
         back.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(), Home.class));
             finish();
+        });
+
+        //logout Button
+        btnLogout.setOnClickListener(view -> {
+            firebaseAuth.signOut();
         });
 
     }
